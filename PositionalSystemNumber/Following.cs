d51ed9e9539474cc636b90d8ee6ca33d80adb403
hizam37 <97097637+hizam37@users.noreply.github.com>
2022-01-14 12:44:08 +0300

    Add files via upload

---
+++ PositionalSystemNumber/Following.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositionalSystemNumber
+{
+    internal class Following
+    {
+        static void Main(string[] args)
+        {
+            IConvert a = new DecimalConverter();//Объявим экземпляр интерфейса.
+            a.Convert(); //Вызовем член
+        }
+    }
+}
